35768b57c4bc9b2021cf3b1dd2ae05215ced8b98
efeozyesil <efeozyesil@gmail.com>
2025-07-04 22:55:15 +0300

    Create research.js

---
+++ src/src/managers/src/data/src/data/src/data/research.js
@@ -0,0 +1,10 @@
+export const researchTree = [
+  {
+    id: 'agriculture',
+    name: 'Agriculture',
+    tier: 1,
+    cost: { food: 100 },
+    effect: { unlockBuildings: ['farm'] }
+  },
+  // ... add more research nodes per tier
+];
