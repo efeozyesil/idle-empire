9c9a49ab3e929e3388b9df21708d47ddc89d4888
efeozyesil <efeozyesil@gmail.com>
2025-07-04 22:54:39 +0300

    Create resources.js

---
+++ src/src/managers/src/data/resources.js
@@ -0,0 +1,5 @@
+export const resources = [
+  { id: 'wood',   name: 'Wood', tier: 1, amount: 0, perSecond: 0 },
+  { id: 'food',   name: 'Food', tier: 1, amount: 0, perSecond: 0 },
+  // ... add more as you expand tiers up to tier 10
+];
